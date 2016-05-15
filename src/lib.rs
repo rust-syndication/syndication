3680f0ba72e7399dafbc79c57b5c671560ec9f62
Caleb Jones <self@calebjones.net>
2016-05-14 23:59:57 -0400

    Add struct definition for common feed format

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,65 @@
 extern crate atom_syndication;
 extern crate rss;
+extern crate chrono;
 
 use std::str::FromStr;
+use chrono::{DateTime, UTC};
 
-pub enum Feed {
+pub struct Entry { }
+pub struct Category { }
+pub struct Feed {
+    // If created from an RSS or Atom feed, this is the original contents
+    source_data: Option<FeedData>,
+    // `id` in Atom, not present in RSS
+    pub id: Option<String>,
+    // `title` in both Atom and RSS
+    pub title: String,
+    // `subtitle` in Atom, and `description` in RSS (required)
+    pub description: Option<String>,
+    // `updated` in Atom (required), and `pub_date` or `last_build_date` in RSS
+    // TODO: Document which RSS field is preferred
+    // This field is required in Atom, but optional in RSS
+    pub updated: Option<DateTime<UTC>>,
+    // `rights` in Atom, and `copyright` in RSS
+    pub copyright: Option<String>,
+    // `icon` in Atom, and `image` in Atom
+    pub icon: Option<String>,
+    // `links` in Atom, and `link` in RSS (will produce a Vec of 1 item)
+    pub links: Vec<String>,
+    // `categories` in both Atom and RSS
+    pub categories: Vec<Category>,
+    // `authors` in Atom, not present in RSS (RSS will produce an empty Vec)
+    pub authors: Vec<atom_syndication::Person>,
+    // `contributors` in Atom, not present in RSS (produces an empty Vec)
+    pub contributors: Vec<atom_syndication::Person>,
+    // `entries` in Atom, and `items` in RSS
+    pub entries: Vec<Entry>,
+}
+
+enum FeedData {
     Atom(atom_syndication::Feed),
     RSS(rss::Channel),
 }
 
-impl FromStr for Feed {
+impl FromStr for FeedData {
     type Err = &'static str;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.parse::<atom_syndication::Feed>() {
-            Ok (feed) => Ok (Feed::Atom(feed)),
+            Ok (feed) => Ok (FeedData::Atom(feed)),
             _ => match s.parse::<rss::Rss>() {
-                Ok (rss::Rss(channel)) => Ok (Feed::RSS(channel)),
+                Ok (rss::Rss(channel)) => Ok (FeedData::RSS(channel)),
                 _ => Err ("Could not parse XML as Atom or RSS from input")
             }
         }
     }
 }
 
-impl ToString for Feed {
+impl ToString for FeedData {
     fn to_string(&self) -> String {
         match self {
-            &Feed::Atom(ref atom_feed) => atom_feed.to_string(),
-            &Feed::RSS(ref rss_channel) => rss::Rss(rss_channel.clone()).to_string(),
+            &FeedData::Atom(ref atom_feed) => atom_feed.to_string(),
+            &FeedData::RSS(ref rss_channel) => rss::Rss(rss_channel.clone()).to_string(),
         }
     }
 }
@@ -40,7 +73,7 @@ mod test {
     use std::io::Read;
     use std::str::FromStr;
 
-    use super::Feed;
+    use super::FeedData;
 
     // Source: https://github.com/vtduncan/rust-atom/blob/master/src/lib.rs
     #[test]
@@ -48,7 +81,7 @@ mod test {
         let mut file = File::open("test-data/atom.xml").unwrap();
         let mut atom_string = String::new();
         file.read_to_string(&mut atom_string).unwrap();
-        let feed = Feed::from_str(&atom_string).unwrap();
+        let feed = FeedData::from_str(&atom_string).unwrap();
         assert!(feed.to_string().len() > 0);
     }
 
@@ -58,7 +91,7 @@ mod test {
         let mut file = File::open("test-data/rss.xml").unwrap();
         let mut rss_string = String::new();
         file.read_to_string(&mut rss_string).unwrap();
-        let rss = Feed::from_str(&rss_string).unwrap();
+        let rss = FeedData::from_str(&rss_string).unwrap();
         assert!(rss.to_string().len() > 0);
     }
 
@@ -76,7 +109,7 @@ mod test {
             ..Default::default()
         };
 
-        let feed = Feed::Atom(atom_syndication::Feed {
+        let feed = FeedData::Atom(atom_syndication::Feed {
             title: "My Blog".to_string(),
             authors: vec![author],
             entries: vec![entry],
@@ -104,7 +137,7 @@ mod test {
             ..Default::default()
         };
 
-        let rss = Feed::RSS(channel);
+        let rss = FeedData::RSS(channel);
         assert_eq!(rss.to_string(), "<?xml version=\'1.0\' encoding=\'UTF-8\'?><rss version=\'2.0\'><channel><title>My Blog</title><link>http://myblog.com</link><description>Where I write stuff</description><item><title>My first post!</title><link>http://myblog.com/post1</link><description>This is my first post</description></item></channel></rss>");
     }
 }
